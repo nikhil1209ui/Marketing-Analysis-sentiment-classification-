8feeba9286c2df3686b56feaf3c3a41ac131589f
Nikhil kushwaha <135628014+nikhil1209ui@users.noreply.github.com>
2025-01-17 15:22:12 +0530

    Update fact_engagement_data.sql

--- SQL_Queries/fact_engagement_data.sql
+++ SQL_Queries/fact_engagement_data.sql
@@ -1,10 +1,10 @@
 -- Query to clean and normalize the engagement_data table
 
 SELECT 
-    EngagementID,  -- Selects the unique identifier for each engagement record
-    ContentID,  -- Selects the unique identifier for each piece of content
-	CampaignID,  -- Selects the unique identifier for each marketing campaign
-    ProductID,  -- Selects the unique identifier for each product
+    EngagementID, 
+    ContentID, 
+	CampaignID,  
+    ProductID,  
     UPPER(REPLACE(ContentType, 'Socialmedia', 'Social Media')) AS ContentType,  -- Replaces "Socialmedia" with "Social Media" and then converts all ContentType values to uppercase
     LEFT(ViewsClicksCombined, CHARINDEX('-', ViewsClicksCombined) - 1) AS Views,  -- Extracts the Views part from the ViewsClicksCombined column by taking the substring before the '-' character
     RIGHT(ViewsClicksCombined, LEN(ViewsClicksCombined) - CHARINDEX('-', ViewsClicksCombined)) AS Clicks,  -- Extracts the Clicks part from the ViewsClicksCombined column by taking the substring after the '-' character
@@ -12,6 +12,6 @@ SELECT
     -- Converts the EngagementDate to the dd.mm.yyyy format
     FORMAT(CONVERT(DATE, EngagementDate), 'dd.MM.yyyy') AS EngagementDate  -- Converts and formats the date as dd.mm.yyyy
 FROM 
-    dbo.engagement_data  -- Specifies the source table from which to select the data
+    dbo.engagement_data  
 WHERE 
     ContentType != 'Newsletter';  -- Filters out rows where ContentType is 'Newsletter' as these are not relevant for our analysis
