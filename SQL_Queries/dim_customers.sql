fa1106cb9cf853df6c73b9cfdd29c304418178dc
Nikhil kushwaha <135628014+nikhil1209ui@users.noreply.github.com>
2025-01-25 15:46:26 +0530

    Update dim_customers.sql

--- SQL_Queries/dim_customers.sql
+++ SQL_Queries/dim_customers.sql
@@ -11,9 +11,6 @@ SELECT
 FROM 
     dbo.customers as c  -- Specifies the alias 'c' for the dim_customers table
 LEFT JOIN
--- RIGHT JOIN
--- INNER JOIN
--- FULL OUTER JOIN
     dbo.geography g  -- Specifies the alias 'g' for the dim_geography table
 ON 
     c.GeographyID = g.GeographyID; 
